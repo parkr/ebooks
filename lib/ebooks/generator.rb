9a24fb7b5888bbfb1ac2db7d77894ecc340c9092
pikesley <github@orgraphone.org>
2015-02-02 17:19:45 +0000

    This is going away

--- lib/ebooks/generator.rb
+++ lib/ebooks/generator.rb
@@ -4,22 +4,18 @@ module Ebooks
     attr_accessor :dictionary
 
     def initialize(config_file = '~/.ebooks')
-      @config = Ebooks.read_config_file config_file
+      @config = Config.new config_file
 
-      @tweets_csv_path = @config[:tweets_csv_path]
-      @corpus_path     = @config[:corpus_path]
       build_corpus
-      @dictionary_name = @config[:dictionary_name]
-      @dictionary      = build_dictionary
     end
 
     def generate_twitter_corpus
       # Go to Twitter.com -> Settings -> Download Archive.
       # This tweets.csv file is in the top directory. Put it in the same directory as this script.
-      csv_text = CSV.read(@tweets_csv_path)[1..-1]
+      csv_text = CSV.read(@config[:tweets_csv_path])[1..-1]
 
       # Create a new clean file of text that acts as the seed for your Markov chains
-      File.open(@corpus_path, 'w') do |file|
+      File.open(@config[:corpus_path], 'w') do |file|
         csv_text.reverse_each do |row|
           tweet_text = row[5]
                         .gsub(/(?:f|ht)tps?:\/[^\s]+/, '') # Strip links
@@ -48,24 +44,24 @@ module Ebooks
     private
 
     def build_corpus
-      unless File.exists?(@corpus_path)
+      unless File.exists?(@config[:corpus_path])
         generate_twitter_corpus
       end
     end
 
-    def build_dictionary
+    def dictionary
       if File.exists?(dictionary_path)
-        MarkyMarkov::Dictionary.new(@dictionary_name)
+        MarkyMarkov::Dictionary.new(@config[:dictionary_name])
       else
-        markov = MarkyMarkov::Dictionary.new(@dictionary_name)
-        markov.parse_file(@corpus_path)
+        markov = MarkyMarkov::Dictionary.new(@config[:dictionary_name])
+        markov.parse_file(@config[:corpus_path])
         markov.save_dictionary!
         markov
       end
     end
 
     def dictionary_path
-      "#{@dictionary_name}.mmd"
+      "#{@config[:dictionary_name]}.mmd"
     end
 
   end
