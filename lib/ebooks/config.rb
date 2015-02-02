c19e91f27c62ca19954cdc2cbc38f7026aaaaf80
pikesley <github@orgraphone.org>
2015-02-02 17:18:52 +0000

    Config object

---
+++ lib/ebooks/config.rb
@@ -0,0 +1,27 @@
+module Ebooks
+  class Config
+    def initialize conf_file = nil
+      @conf = YAML.load(
+                File.open(
+                  File.join(
+                    File.dirname(__FILE__),
+                                 '..',
+                                 '..',
+                                 'config/defaults.yaml'
+                            )
+                          )
+                        )
+
+      if conf_file
+        conf_file.sub!('~', ENV["HOME"])
+        @overrides = YAML.load(File.open(conf_file))
+        @conf = @conf.deep_merge @overrides
+      end
+
+    end
+
+    def [] key
+      @conf[key]
+    end
+  end
+end
---
+++ spec/config_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+module Ebooks
+  describe Config do
+    it 'has defaults' do
+      @c = Config.new
+      expect(@c[:tweets_csv_path]).to eq 'tweets.csv'
+      expect(@c[:corpus_path]).to eq 'markov_dict.txt'
+    end
+
+    it 'can absorb overrides' do
+      @c = Config.new File.join(File.dirname(__FILE__), '..', 'features/support/fixtures/config/ebooks')
+      expect(@c[:tweets_csv_path]).to eq 'tmp/aruba/cukes/ebooks/tweets.csv'
+    end
+  end
+end
