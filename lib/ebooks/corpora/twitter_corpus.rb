c62f685b3e8bbf952d0d1055d7d8774409486b0e
pikesley <github@orgraphone.org>
2015-02-02 17:19:27 +0000

    Corpus as object

---
+++ lib/ebooks/corpora/twitter_corpus.rb
@@ -0,0 +1,42 @@
+module Ebooks
+  module Corpora
+    class TwitterCorpus
+      def initialize config
+        @config = config
+      end
+
+      def generate
+        FileUtils.mkdir_p File.dirname @config[:corpus_path]
+        FileUtils.touch @config[:corpus_path]
+      end
+
+      def csv
+        @csv ||= CSV.read(@config[:tweets_csv_path])[1..-1]
+      end
+
+      def self.filter tweet
+        self.compress_spaces self.strip_rts self.strip_usernames self.strip_newlines self.strip_links tweet
+      end
+
+      def self.strip_links tweet
+        tweet.gsub(/(?:f|ht)tps?:\/[^\s]+/, '').strip
+      end
+
+      def self.strip_newlines tweet
+        tweet.gsub(/\n/,' ').gsub(/\s+/, ' ')
+      end
+
+      def self.strip_usernames tweet
+        tweet.gsub(/@[a-z0-9_]+/i, '').strip
+      end
+
+      def self.strip_rts tweet
+        tweet.gsub(/[R|M]T/, '').strip
+      end
+
+      def self.compress_spaces tweet
+        tweet.gsub(/\s+/, ' ').strip
+      end
+    end
+  end
+end
---
+++ spec/corpora/twitter_corpus_spec.rb
@@ -0,0 +1,52 @@
+require 'spec_helper'
+
+module Ebooks
+  module Corpora
+    describe TwitterCorpus do
+      before :each do
+        @c = Config.new File.join(File.dirname(__FILE__), '..', 'fixtures/config/ebooks')
+        @tc = TwitterCorpus.new @c
+      end
+
+      it 'produces an output file' do
+        @tc.generate
+        expect(File).to exist 'spec/fixtures/ebooks/markov_dict.txt'
+      end
+
+      it 'reads the tweet CSV file' do
+        csv = @tc.csv
+        expect(csv[0][5]).to match /rspec/
+        expect(csv[1][5]).to match /updog/
+      end
+
+      context 'filter the tweet' do
+        it 'strips links' do
+          expect(TwitterCorpus.strip_links 'Link to http://whatever.com').to eq 'Link to'
+        end
+
+        it 'strips newlines' do
+          expect(TwitterCorpus.strip_newlines 'This
+          has a newline').to eq 'This has a newline'
+        end
+
+        it 'strips usernames' do
+          expect(TwitterCorpus.strip_usernames 'Sam @pikesley').to eq 'Sam'
+        end
+
+        it 'strips RTs' do
+          expect(TwitterCorpus.strip_rts 'RT This is a retweet').to eq 'This is a retweet'
+        end
+
+        it 'compresses spaces' do
+          expect(TwitterCorpus.compress_spaces ' This    has extraneous   spaces').to eq 'This has extraneous spaces'
+        end
+
+        it 'applies all the filters' do
+          tweet = 'RT Link from @pikesley to https://pikesley.org with
+          newlines '
+          expect(TwitterCorpus.filter tweet).to eq 'Link from to with newlines'
+        end
+      end
+    end
+  end
+end
