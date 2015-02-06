ede530966452f65efa139b4238893d86f89df8d9
pikesley <github@orgraphone.org>
2015-02-06 15:16:16 +0000

    Formatting

--- lib/ebooks/cli.rb
+++ lib/ebooks/cli.rb
@@ -9,14 +9,20 @@ module Ebooks
     map %w(-v --version) => :version
 
     desc 'generate', 'Generate a tweet'
-    method_option :config, :aliases => '-c', :desc => 'Specify config file', :default => '~/.ebooks'
+    method_option :config,
+                  :aliases => '-c',
+                  :desc => 'Specify config file',
+                  :default => '~/.ebooks'
     def generate
       g = Generator.new options[:config]
       puts g
     end
 
     desc 'tweet', 'Tweet a tweet'
-    method_option :config, :aliases => '-c', :desc => 'Specify config file', :default => '~/.ebooks'
+    method_option :config,
+                  :aliases => '-c',
+                  :desc => 'Specify config file',
+                  :default => '~/.ebooks'
     def tweet
       g = Generator.new options[:config]
       g.tweet
